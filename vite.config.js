67baa4cf61a45eb9f7670d2b2a6880bfa02bfbfd
Jeyreet <jeyreet@yandex.ru>
2025-10-05 22:11:11 +0700

    Added aliases
    Several folder aliases have been added

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 import sassDts from 'vite-plugin-sass-dts'
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), sassDts()],
   resolve: {
     alias: {
-      "@src": path.resolve(__dirname, "src"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@types": path.resolve(__dirname, "src/types.ts"),
-      "@utils": path.resolve(__dirname, "src/utils"),
-      "@styles": path.resolve(__dirname, "src/styles"),
-      "@mixins": path.resolve(__dirname, "src/styles/mixins.scss"),
+      '@src': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@ui': path.resolve(__dirname, 'src/components/ui'),
+      '@blocks': path.resolve(__dirname, 'src/components/blocks'),
+      '@pages': path.resolve(__dirname, 'src/components/pages'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@types': path.resolve(__dirname, 'src/types.ts'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@mixins': path.resolve(__dirname, 'src/styles/mixins.scss'),
     },
   },
-  base: 'timers'
+  base: 'timers',
 })
