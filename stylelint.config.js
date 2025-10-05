013868eb4261824478bb442cee137b3978460f83
Jeyreet <jeyreet@yandex.ru>
2025-10-05 22:02:55 +0700

    Modified Stylelint config
    The class naming convention in Stylelint has been changed to camelCase

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,11 @@
 export default {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
+  rules: {
+    'selector-class-pattern': [
+      '^[a-z][a-zA-Z0-9]*$',
+      {
+        message: (className) => `Expected class selector "${className}" to be camelCase`,
+      },
+    ],
+  },
 }
