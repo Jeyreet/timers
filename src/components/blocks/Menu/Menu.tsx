931586b92a6011b051aefc85d5682c4b86285de9
Jeyreet <jeyreet@yandex.ru>
2025-10-06 19:03:37 +0700

    Added user info to menu
    Menu now has user name and user image

--- src/components/blocks/Menu/Menu.tsx
+++ src/components/blocks/Menu/Menu.tsx
@@ -1,7 +1,16 @@
+import userpic from '@assets/images/user.webp?srcset'
+import Picture from '@ui/Picture'
 import styles from './Menu.module.scss'
 
 const Menu = () => {
-  return <div className={styles.menu}>Menu</div>
+  return (
+    <div className={styles.menu}>
+      <div className={styles.user}>
+        <Picture className={styles.userpic} image={userpic} />
+        <div className={styles.username}>Имя пользователя</div>
+      </div>
+    </div>
+  )
 }
 
 export default Menu
