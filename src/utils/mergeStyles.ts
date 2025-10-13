dd0479f7559f78b36a433144f80bc8717073a71d
Jeyreet <jeyreet@yandex.ru>
2025-10-13 14:19:09 +0700

    Refactor mergeStyles function to improve type safety by specifying extraStylesArray type

--- src/utils/mergeStyles.ts
+++ src/utils/mergeStyles.ts
@@ -1,10 +1,12 @@
-const mergeStyles = <T extends Record<string, string>>(defaultStyles: T, ...extraStylesArray: Array<Partial<T>>) => {
+const mergeStyles = <T extends Record<string, string>>(defaultStyles: T, ...extraStylesArray: Array<Partial<Record<keyof T, string>> | undefined>) => {
   const styles: Record<string, string> = {}
 
   for (const [key, style] of Object.entries(defaultStyles)) {
     styles[key] = style
 
     extraStylesArray.forEach(extraStyles => {
+      if (!extraStyles) return
+
       if (key in extraStyles) {
         styles[key] += ` ${extraStyles[key]}`
       }
